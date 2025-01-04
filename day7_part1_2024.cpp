dbd57ca3b64595750a5899a18b93eb9904f931c8
Galina <djngalja@gmail.com>
2025-01-04 15:21:40 +0300

    Update day7_part1_2024.cpp
    Improved readability

--- day7_part1_2024.cpp
+++ day7_part1_2024.cpp
@@ -14,45 +14,35 @@ bool eval_equation (Equation);
 void day7_part1();
 
 
-
-
-
 int main() {
     day7_part1();
     return 0;
 }
 
 
-
-
 void day7_part1() {
     std::vector<Equation> vec;
     read_equations(vec);
-
-    long long int result = 0;
+    long long int result {0};
     for (auto eq: vec) {
         if (eval_equation(eq)) {
             result += eq.test_value;
         }
     }
-    std::cout << "total calibration result: " << result;
+    std::cout << "Total calibration result: " << result;
 }
 
-
-
 bool eval_equation (Equation eq) {
-    bool result = false;
     while (eq.nums.size() > 1) {
-        if (eq.test_value%eq.nums[eq.nums.size()-1] == 0) {
-            Equation new_eq = eq;
-            new_eq.test_value /= new_eq.nums[new_eq.nums.size()-1];
+        if (eq.test_value%eq.nums.back() == 0) { //division possible
+            Equation new_eq {eq};
+            new_eq.test_value /= new_eq.nums.back();
             new_eq.nums.pop_back();
-            result = eval_equation(new_eq);
-            if (result) {
+            if (eval_equation(new_eq)) {
                 return true;
             }
         }
-        eq.test_value -= eq.nums[eq.nums.size()-1];
+        eq.test_value -= eq.nums.back();
         eq.nums.pop_back();
     }
     if (eq.test_value == eq.nums[0]) {
@@ -61,8 +51,6 @@ bool eval_equation (Equation eq) {
     return false;
 }
 
-
-
 void read_equations(std::vector<Equation>& vec) {
     std::ifstream file("advent_day7.txt");
     if (!file) {
@@ -70,16 +58,16 @@ void read_equations(std::vector<Equation>& vec) {
     }
     std::string str;
     while (getline(file, str)) {
-        std::size_t i = 0;
+        std::size_t i {0};
         std::string temp;
-        std::vector<int> temp_v;
-        Equation e;
         while (str[i] != ':') {
             temp += str[i];
             i++;
         }
+        Equation e;
         e.test_value = stoll(temp);
         temp.clear();
+        std::vector<int> temp_v;
         while (i != str.size()) {
             if(isdigit(str[i])) {
                 temp += str[i];
