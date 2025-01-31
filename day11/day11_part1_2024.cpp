73af89ebe1c3a148703c1b4bfd120b388f3768bf
Galina <djngalja@gmail.com>
2025-01-31 19:43:49 +0300

    Update day11_part1_2024.cpp
    Slightly improved readability

--- day11/day11_part1_2024.cpp
+++ day11/day11_part1_2024.cpp
@@ -4,13 +4,13 @@
 #include <string>
 #include <cmath>
 
-struct Stones { long long int stone1; long long int stone2; };
+struct Stones { long long st1; long long st2; };
 
 void day11_part1();
-bool read_puzzle_input(const std::string&, std::vector<long long int>&);
-int digit_num(long long int);
-Stones split_stone(long long int, int);
-void blink(std::vector<long long int>&);
+bool read_puzzle_input(const std::string&, std::vector<long long>&);
+int digit_num(long long);
+Stones split_stone(long long, int);
+void blink(std::vector<long long>&);
 
 
 int main() {
@@ -20,14 +20,14 @@ int main() {
 
 
 void day11_part1() {
-    std::vector<long long int> stones;
+    std::vector<long long> stones;
     if (read_puzzle_input("input.txt", stones)) {
         for (int i=0; i<25; ++i) { blink(stones); }
         std::cout << stones.size() << " stones\n";
     }
 }
 
-bool read_puzzle_input(const std::string& f_name, std::vector<long long int>& stones) {
+bool read_puzzle_input(const std::string& f_name, std::vector<long long>& stones) {
     std::ifstream file(f_name);
     if (!file) {
         std::cout << "File <" << f_name << "> not found\n";
@@ -39,7 +39,7 @@ bool read_puzzle_input(const std::string& f_name, std::vector<long long int>& st
     return true;
 }
 
-int digit_num(long long int num) {
+int digit_num(long long num) {
     int cnt {};
     while (num) {
         num/= 10;
@@ -48,21 +48,21 @@ int digit_num(long long int num) {
     return cnt;
 }
 
-Stones split_stone(long long int stone, int digit_num) {
-    long long int div = std::pow(10, digit_num/2);
-    long long int stone1 = stone / div ;
-    long long int stone2 = stone % div ;
-    return {stone1, stone2};
+Stones split_stone(long long stone, int digit_num) {
+    long long div = std::pow(10, digit_num/2);
+    long long st1 = stone / div ;
+    long long st2 = stone % div ;
+    return {st1, st2};
 }
 
-void blink(std::vector<long long int>& stones) {
-    std::vector<long long int> new_stones;
-    for (const long long int& stone : stones) {
+void blink(std::vector<long long>& stones) {
+    std::vector<long long> new_stones;
+    for (const auto& stone : stones) {
         if (stone == 0) { new_stones.push_back(1); }
         else if (digit_num(stone)%2 == 0) {
-            Stones stones = split_stone(stone, digit_num(stone));
-            new_stones.push_back(stones.stone1);
-            new_stones.push_back(stones.stone2);
+            Stones stone_pair = split_stone(stone, digit_num(stone));
+            new_stones.push_back(stone_pair.st1);
+            new_stones.push_back(stone_pair.st2);
         } else {
             new_stones.push_back(stone*2024);
         }
