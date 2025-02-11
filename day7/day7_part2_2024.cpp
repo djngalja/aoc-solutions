79b7d42e66fd95a41e3b960173b8ffadfb904bdf
Galina <djngalja@gmail.com>
2025-02-11 15:51:28 +0300

    Update day7_part2_2024.cpp
    Improved performance

--- day7/day7_part2_2024.cpp
+++ day7/day7_part2_2024.cpp
@@ -2,16 +2,13 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <cctype> //isdigit()
+#include <cctype> // std::isdigit
+#include <cmath> // std::pow
 
-struct Equation { long long int test_value; std::vector<long long int> nums; };
-
-auto sum(long long int, long long int);
-auto prod(long long int, long long int);
-auto conc(long long int, long long int);
-void read_equations(std::vector<Equation>&);
-bool eval_equation(Equation);
-bool eval_equation2(Equation&);
+int calc_divisor(long long);
+bool eval_equation (std::vector<long long>);
+std::vector<long long> split_str(const std::string&);
+bool read_equations(const std::string&, std::vector<std::vector<long long>>&);
 void day7_part2();
 
 
@@ -22,113 +19,77 @@ int main() {
 
 
 void day7_part2() {
-    std::vector<Equation> vec;
-    read_equations(vec);
-    long long int result {0};
-    int counter {0};
-    for (auto& eq : vec) {
-        if (++counter%10 == 0) {
-            std::cout << "Progress: " << counter << " out of " << vec.size() << '\n';
+    std::vector<std::vector<long long>> equations;
+    if (read_equations("input.txt", equations)) {
+        long long result {};
+        for (const auto& eq : equations) {
+            if (eval_equation(eq)) {
+                result += eq[0]; // the 1st element is test value
+            }
         }
-        if (eval_equation(eq)) {
-            result += eq.test_value;
-        }
-        else {
-            if (eval_equation2(eq)) {
-                result += eq.test_value;
+        std::cout << "The new total calibration result: " << result << '\n';
+    }
+}
+
+bool read_equations(const std::string& f_name, std::vector<std::vector<long long>>& equations) {
+    std::ifstream file(f_name);
+    if (!file) {
+        std::cout << "File <" << f_name << "> not found\n";
+        return false;
+    }
+    std::string temp_str;
+    while (getline(file, temp_str)) {
+        equations.push_back(split_str(temp_str));
+    }
+    return true;
+}
+
+std::vector<long long> split_str(const std::string& str) {
+    std::vector<long long> temp_vec;
+    std::string temp_str;
+    for (char c : str) {
+        if (isdigit(c)) {
+            temp_str += c;
+        } else {
+            if (!temp_str.empty()) {
+                temp_vec.push_back(stoll(temp_str));
+                temp_str.clear();
             }
         }
     }
-    std::cout << "\nTotal calibration result: " << result;
+    temp_vec.push_back(stoll(temp_str));
+    return temp_vec;
 }
 
-auto sum(long long int a, long long int b) {
-    return a + b;
-}
-
-auto prod(long long int a, long long int b) {
-    return a * b;
-}
-
-auto conc(long long int a, long long int b) {
-    std::string a_str = std::to_string(a);
-    std::string b_str = std::to_string(b);
-    return stoll(a_str + b_str);
-}
-
-bool eval_equation2(Equation& eq) {
-    while (eq.nums.size() > 1) {
-        auto s = sum(eq.nums[0], eq.nums[1]);
-        auto p = prod(eq.nums[0], eq.nums[1]);
-        auto c = conc(eq.nums[0], eq.nums[1]);
-        eq.nums.erase(eq.nums.begin(), eq.nums.begin()+2);
-        Equation eq2 {eq};
-        eq2.nums.insert(eq2.nums.begin(), p);
-        if (eval_equation2(eq2)) {
-            return true;
-        }
-        Equation eq3 {eq};
-        eq3.nums.insert(eq3.nums.begin(), c);
-        if (eval_equation2(eq3)) {
-            return true;
-        }
-        eq.nums.insert(eq.nums.begin(), s);
-    }
-    if (eq.test_value == eq.nums[0]) {
-        return true;
-    }
-    return false;
-}
-
-bool eval_equation (Equation eq) {
-    while (eq.nums.size() > 1) {
-        if (eq.test_value%eq.nums.back() == 0) { //division possible
-            Equation new_eq {eq};
-            new_eq.test_value /= new_eq.nums.back();
-            new_eq.nums.pop_back();
+bool eval_equation (std::vector<long long> eq) {
+    while (eq.size() > 2) {
+        if (eq[0] % eq.back() == 0) { // division is possible
+            std::vector<long long> new_eq {eq};
+            new_eq[0] /= new_eq.back();
+            new_eq.pop_back();
             if (eval_equation(new_eq)) {
                 return true;
             }
         }
-        eq.test_value -= eq.nums.back();
-        eq.nums.pop_back();
+        if (eq[0] % calc_divisor(eq.back()) == eq.back()) { // concatenation is possible
+            std::vector<long long> new_eq {eq};
+            new_eq[0] /= calc_divisor(new_eq.back());
+            new_eq.pop_back();
+            if (eval_equation(new_eq)) {
+                return true;
+            }
+        }
+        eq[0] -= eq.back();
+        eq.pop_back();
     }
-    if (eq.test_value == eq.nums[0]) {
-        return true;
-    }
-    return false;
+    return eq[0] == eq[1];
 }
 
-void read_equations(std::vector<Equation>& vec) {
-    std::ifstream file("input.txt");
-    if (!file) {
-        std::cout << "File not found\n";
+int calc_divisor(long long last_num) {
+    int exp{};
+    while(last_num) {
+        last_num /= 10;
+        exp++;
     }
-    std::string str;
-    while (getline(file, str)) {
-        std::size_t i {0};
-        std::string temp;
-        while (str[i] != ':') {
-            temp += str[i];
-            i++;
-        }
-        Equation e;
-        e.test_value = stoll(temp);
-        temp.clear();
-        std::vector<long long int> temp_v;
-        while (i != str.size()) {
-            if(isdigit(str[i])) {
-                temp += str[i];
-            } else {
-                if (!temp.empty()) {
-                    temp_v.push_back(stoi(temp));
-                    temp.clear();
-                }
-            }
-            i++;
-        }
-        temp_v.push_back(stoi(temp));
-        e.nums = temp_v;
-        vec.push_back(e);
-    }
+    return pow(10, exp);
 }
