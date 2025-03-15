3e7511aa86bb4a0d47ab7b6f9b7abd14070a77ef
Galina <djngalja@gmail.com>
2025-03-15 22:48:24 +0300

    Create day3_part2_2024.cpp
    Advent of Code 2024
    Day 3 (part 2)
    Solved in C++, no AI used

---
+++ day3/day3_part2_2024.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+
+/* Returns a pair consisting of the puzzle answer
+and a bool value set to false if the puzzle input couldn't be found */
+std::pair<int, bool> read_file(const std::string&);
+
+
+int main() {
+    auto result = read_file("input.txt");
+    if (result.second) {
+        std::cout << "Result: " << result.first << '\n';
+    }
+    return 0;
+}
+
+
+std::pair<int, bool> read_file(const std::string& f_name) {
+    std::ifstream file(f_name);
+    if (!file) {
+        std::cout << "File <" << f_name << "> not found.\n";
+        return {0, false};
+    }
+    int result {};
+    std::regex pat {R"(mul\((\d+),(\d+)\)|do(n't)?\(\))"};
+    bool enabled {true};
+    for (std::string str; getline(file, str);) {
+        for (std::sregex_iterator p(str.begin(), str.end(), pat); p!=std::sregex_iterator{}; ++p) {
+            std::string match = (*p)[0];
+            if (match == "do()") {
+                enabled = true;
+            } else if (match == "don't()") {
+                enabled = false;
+            } else {
+                if (enabled) {
+                    result += stoi((*p)[1]) * stoi((*p)[2]);
+                }
+            }
+        }
+    }
+    return {result, true};
+}
