0a3c69386ff8494d2739a9ba331bf790bc03d730
Galina <djngalja@gmail.com>
2025-03-15 21:54:47 +0300

    Create day3_part1_2024.cpp
    Advent of Code 2024
    Day 3 (part 1)
    Solved in C++, no AI used

---
+++ day3/day3_part1_2024.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+
+/* Returns a pair consisting of the puzzle answer
+and a bool value set to false if the puzzle input couldn't be found */
+std::pair<int, bool> read_file(const std::string&);
+
+
+int main() {
+    auto result = read_file("input.txt");
+    if (result.second) {
+        std::cout << "Result: " << result.first << '\n';
+    }
+    return 0;
+}
+
+
+std::pair<int, bool> read_file(const std::string& f_name) {
+    std::ifstream file(f_name);
+    if (!file) {
+        std::cout << "File <" << f_name << "> not found\n";
+        return {0, false};
+    }
+    int result {};
+    std::regex pat {R"(mul\((\d+),(\d+)\))"};
+    for (std::string line; getline(file, line);) {
+        for (std::sregex_iterator p(line.begin(), line.end(), pat); p!=std::sregex_iterator{}; ++p) {
+            result += stoi((*p)[1]) * stoi((*p)[2]);
+        }
+    }
+    return {result, true};
+}
