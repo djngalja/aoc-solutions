5252867176b011c80250c2c35a9e50d69e4e28ba
Galina <djngalja@gmail.com>
2025-06-01 16:16:23 +0300

    Create day19_part1_2024.cpp

---
+++ day19/day19_part1_2024.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+
+bool is_possible(const std::set<std::string>&, const std::string&, std::map<std::string, bool>&);
+std::set<std::string> get_patterns(const std::string&);
+bool read_input(const std::string&, std::set<std::string>&, std::vector<std::string>&);
+
+
+int main() {
+    std::set<std::string> patterns;
+    std::vector<std::string> designs;
+    if (read_input("input.txt", patterns, designs)) {
+        int cnt {};
+        std::map<std::string, bool> cache;
+        for (const auto& design : designs) {
+            if (is_possible(patterns, design, cache)) {
+                cnt++;
+            }
+        }
+        std::cout << "Result: " << cnt << '\n';
+    }
+    return 0;
+}
+
+
+bool is_possible(const std::set<std::string>& patterns, const std::string& design, std::map<std::string, bool>& cache) {
+    if (auto res = cache.find(design); res != cache.end()) {
+        return res -> second;
+    }
+    if (patterns.find(design) != patterns.end()) {
+        cache[design] = true;
+        return true;
+    }
+    for (std::size_t i = 1; i < design.size(); ++i) {
+        if (!is_possible(patterns, design.substr(0, i), cache)) {
+            continue;
+        }
+        if (is_possible(patterns, design.substr(i, design.size() - i), cache)) {
+            cache[design] = true;
+            return true;
+        }
+    }
+    cache[design] = false;
+    return false;
+}
+
+std::set<std::string> get_patterns(const std::string& str) {
+    std::set<std::string> patterns;
+    std::string temp;
+    for (char c : str) {
+        if (std::isalpha(c)) { temp += c; }
+        else {
+            if (temp.empty()) { continue; }
+            patterns.insert(temp);
+            temp.clear();
+        }
+    }
+    if (!temp.empty()) {
+        patterns.insert(temp);
+    }
+    return patterns;
+}
+
+bool read_input(const std::string& f_name, std::set<std::string>& patterns, std::vector<std::string>& designs) {
+    std::ifstream file(f_name);
+    if (!file) {
+        std::cout << "File <" << f_name << "> not found\n";
+        return false;
+    }
+    int cnt {};
+    for (std::string str; getline(file, str);) {
+        cnt++;
+        if (cnt == 1) { patterns = get_patterns(str); }
+        else if (str.empty()) { continue; }
+        else { designs.push_back(str); }
+    }
+    return true;
+}
