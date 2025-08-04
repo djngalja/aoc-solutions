b5323b4acb5a384db5277adabedd224d0849962f
djngalja <djngalja@gmail.com>
2025-08-04 19:09:42 +0300

    Improved readability

--- day23/day23_part1_2024.cpp
+++ day23/day23_part1_2024.cpp
@@ -3,20 +3,18 @@
 #include <string>
 #include <unordered_map>
 #include <set>
-#include <vector>
-#include <algorithm>
 
-bool read_file(const std::string&, 
-    std::unordered_map<std::string, std::vector<std::string>>&);
-void find_inter_connect(const std::unordered_map<std::string, std::vector<std::string>>&, 
-    std::set<std::set<std::string>>&);
-int cnt_t_inter_con(const std::set<std::set<std::string>>&);
+typedef std::set<std::string> SetStr;
+
+bool read_file(const std::string&, std::unordered_map<std::string, SetStr>&);
+void find_inter_connect(const std::unordered_map<std::string, SetStr>&, std::set<SetStr>&);
+int cnt_t_inter_con(const std::set<SetStr>&);
 
 
 int main() {
-    std::unordered_map<std::string, std::vector<std::string>> input;
+    std::unordered_map<std::string, SetStr> input;
     if (read_file("input.txt", input)) {
-        std::set<std::set<std::string>> inter_con_computers;
+        std::set<SetStr> inter_con_computers;
         find_inter_connect(input, inter_con_computers);
         std::cout << cnt_t_inter_con(inter_con_computers) << '\n';
     }
@@ -24,7 +22,7 @@ int main() {
 }
 
 
-bool read_file(const std::string& f_name, std::unordered_map<std::string, std::vector<std::string>>& input) {
+bool read_file(const std::string& f_name, std::unordered_map<std::string, SetStr>& input) {
     std::ifstream file(f_name);
     if (!file) {
         std::cerr << "File <" << f_name << "> not found\n";
@@ -33,19 +31,19 @@ bool read_file(const std::string& f_name, std::unordered_map<std::string, std::v
     for (std::string str; file >> str; ) {
         std::string first = str.substr(0, 2);
         std::string second = str.substr(3);
-        input[first].push_back(second);
-        input[second].push_back(first);
+        input[first].insert(second);
+        input[second].insert(first);
     }
     return true;
 }
 
-void find_inter_connect(const std::unordered_map<std::string, std::vector<std::string>>& input, 
-std::set<std::set<std::string>>& inter_con_computers) {
+void find_inter_connect(const std::unordered_map<std::string, SetStr>& input, 
+std::set<SetStr>& inter_con_computers) {
     for (const auto& connection : input) {
         for (const auto& computer : connection.second) {
             for (const auto& computer2 : input.at(computer)) {
-                if (std::find(connection.second.begin(), connection.second.end(), computer2) != connection.second.end()) {
-                    std::set<std::string> temp{connection.first, computer, computer2};
+                if (connection.second.find(computer2) != connection.second.end()) {
+                    SetStr temp{connection.first, computer, computer2};
                     inter_con_computers.insert(temp);
                 }
             }
@@ -53,7 +51,7 @@ std::set<std::set<std::string>>& inter_con_computers) {
     }
 }
 
-int cnt_t_inter_con(const std::set<std::set<std::string>>& inter_con_computers) {
+int cnt_t_inter_con(const std::set<SetStr>& inter_con_computers) {
     int cnt{};
     for (const auto& connection: inter_con_computers) {
         for (const auto& computer : connection) {
