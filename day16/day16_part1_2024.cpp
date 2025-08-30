3f666f94f28a0f20348964d45d024da60d9147a6
djngalja <djngalja@gmail.com>
2025-08-30 18:11:04 +0300

    Improved readability

--- day16/day16_part1_2024.cpp
+++ day16/day16_part1_2024.cpp
@@ -8,13 +8,21 @@
 typedef std::vector<std::vector<int>> VecVecInts;
 
 struct XY { int x; int y; };
-struct Tile { int score; XY pos; XY dir; };
 bool operator==(const XY&, const XY&);
+XY operator+(const XY&, const XY&);
+
+struct Tile { 
+    int score; 
+    XY pos; 
+    XY dir; 
+};
 bool operator>(const Tile&, const Tile&);
 
-XY rotate90(const XY&, bool = true);
-int calc_lowest_score(VecVecInts&, const XY&, const XY&);
 bool read_input(const std::string& f_name, VecVecInts&, XY&, XY&);
+int calc_lowest_score(VecVecInts&, const XY&, const XY&);
+Tile get_new_tile(VecVecInts&, int, const XY&, const XY&);
+XY rotate90(const XY&, bool);
+
 
 
 int main() {
@@ -28,77 +36,79 @@ int main() {
 }
 
 
+
 bool read_input(const std::string& f_name, VecVecInts& maze_map, XY& s_pos, XY& e_pos) {
     std::ifstream file(f_name);
     if (!file) {
         std::cerr << "File <" << f_name << "> not found.\n";
         return false;
     }
-    int i {}; // Count lines
+    int x {}; 
     for (std::string temp_str; getline(file, temp_str);) {
         std::vector<int> temp_vec;
-        for (size_t j = 0; j < temp_str.size(); ++j) {
-            if (temp_str[j] == '#') {
+        for (size_t y = 0; y < temp_str.size(); ++y) {
+            if (temp_str[y] == '#') {
                 temp_vec.push_back(-1);
-            } else if (temp_str[j] == '.') {
+            } else if (temp_str[y] == '.') {
                 temp_vec.push_back(INT_MAX);
-            } else if (temp_str[j] == 'S') {
+            } else if (temp_str[y] == 'S') {
                 temp_vec.push_back(0); // Start
-                s_pos.x = i;
-                s_pos.y = j;
+                s_pos.x = x;
+                s_pos.y = y;
             } else {
                 temp_vec.push_back(INT_MAX); // End
-                e_pos.x = i;
-                e_pos.y = j;
+                e_pos.x = x;
+                e_pos.y = y;
             }
         }
         maze_map.push_back(temp_vec);
-        i++;
+        ++x;
     }
     return true;
 }
 
+// Using Dijkstra's algorithm
 int calc_lowest_score(VecVecInts& maze_map, const XY& s_pos, const XY& e_pos) {
-    XY init_dir {0, 1}; // Start facing East
+    XY s_dir {0, 1}; // Start facing East
     int lowest_score {};
-    // Using Dijkstra's algorithm
     std::priority_queue<Tile, std::vector<Tile>, std::greater<Tile>> pq;
-    pq.push(Tile{lowest_score, s_pos, init_dir});
+    pq.push(Tile{lowest_score, s_pos, s_dir});
     while (!pq.empty()) {
-        Tile cur = pq.top();
-        if (cur.pos == e_pos) {
-            lowest_score = cur.score;
+        Tile tile = pq.top();
+        if (tile.pos == e_pos) {
+            lowest_score = tile.score;
             break;
         }
         pq.pop();
-        int x = cur.pos.x;
-        int y = cur.pos.y;
-        int dx = cur.dir.x;
-        int dy = cur.dir.y;
-        if (maze_map[x + dx][y + dy] != -1) { // Not a wall
-            if (maze_map[x + dx][y + dy] > cur.score + 1) {
-                maze_map[x + dx][y + dy] = cur.score + 1;
-            }
-            pq.push(Tile{maze_map[x + dx][y + dy], XY{x + dx, y + dy}, cur.dir});
+        Tile new_tile = get_new_tile(maze_map, tile.score, tile.pos, tile.dir);
+        if (new_tile.score) {
+            pq.push(new_tile);
         }
-        for (const XY& new_dir : { rotate90(cur.dir), rotate90(cur.dir, false)}) {
-            dx = new_dir.x;
-            dy = new_dir.y;
-            if (maze_map[x + dx][y + dy] != -1) {
-                if (maze_map[x + dx][y + dy] > cur.score + 1001) {
-                    maze_map[x + dx][y + dy] = cur.score + 1001;
-                }
-                pq.push(Tile{maze_map[x + dx][y + dy], XY{x + dx, y + dy}, new_dir});
+        for (bool cw : { true, false}) {
+            Tile rotated_tile = get_new_tile(
+                maze_map, tile.score + 1000, tile.pos, rotate90(tile.dir, cw));
+            if (rotated_tile.score) {
+                pq.push(rotated_tile);
             }
         }
-        maze_map[x][y] = -1; // Visited
+        maze_map[tile.pos.x][tile.pos.y] = -1; // Visited
     }
     return lowest_score;
 }
 
+Tile get_new_tile(VecVecInts& maze_map, int score, const XY& pos, const XY& dir) {
+    XY new_pos {pos + dir};
+    if (maze_map[new_pos.x][new_pos.y] != -1 && 
+        maze_map[new_pos.x][new_pos.y] > score + 1) { 
+        maze_map[new_pos.x][new_pos.y] = score + 1;
+        return Tile{maze_map[new_pos.x][new_pos.y], new_pos, dir};
+    }
+    return Tile{};
+}
+
 XY rotate90(const XY& dir, bool cw) {
     XY new_dir {};
-    int k = cw ? 1 : -1; // Rotate clockwise by default
+    int k = cw ? 1 : -1; 
     if (dir.x == -1) {
         new_dir.y = k;
     } else if (dir.x == 1) {
@@ -109,10 +119,16 @@ XY rotate90(const XY& dir, bool cw) {
     return new_dir;
 }
 
-bool operator>(const Tile& l, const Tile& r) {
-    return l.score > r.score;
+//XY
+bool operator==(const XY& lhs, const XY& rhs) {
+    return (lhs.x == rhs.x) && (lhs.y == rhs.y);
 }
 
-bool operator==(const XY& l, const XY& r) {
-    return (l.x == r.x) && (l.y == r.y);
+XY operator+(const XY& lhs, const XY& rhs) {
+    return XY{lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+//Tile
+bool operator>(const Tile& lhs, const Tile& rhs) {
+    return lhs.score > rhs.score;
 }
