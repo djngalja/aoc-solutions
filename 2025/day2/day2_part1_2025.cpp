02dfa3afc44cc702edd19420839c073545cce3fa
Galina <djngalja@gmail.com>
2025-12-02 14:05:58 +0300

    Update day2_part1_2025.cpp
    Renamed some variables (resolved a name conflict)

--- 2025/day2/day2_part1_2025.cpp
+++ 2025/day2/day2_part1_2025.cpp
@@ -10,13 +10,13 @@ int main() {
     } else {
         long res{};
         for (std::string temp; getline(file, temp, ',');) {
-            size_t pos = temp.find('-');
-            long end = stol(temp.substr(pos + 1));
-            for (long i = stol(temp.substr(0, pos)); i <= end; ++i) {
+            size_t delim = temp.find('-');
+            long end = stol(temp.substr(delim + 1));
+            for (long i = stol(temp.substr(0, delim)); i <= end; ++i) {
                 std::string num = std::to_string(i);
                 if (num.size() % 2 != 0) { continue; }
-                size_t pos = num.size() / 2;
-                if (num.substr(0, pos) == num.substr(pos)) { 
+                size_t mid = num.size() / 2;
+                if (num.substr(0, mid) == num.substr(mid)) { 
                     res += i;
                 }
             }
