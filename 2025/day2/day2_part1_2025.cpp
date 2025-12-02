fcdca1205d7a70938fb27c6f3129f186b0ff52bd
Galina <djngalja@gmail.com>
2025-12-02 13:52:39 +0300

    Create day2_part1_2025.cpp
    Solved part 1 of day 2 AoC 2025

---
+++ 2025/day2/day2_part1_2025.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string f_name{"input.txt"};
+    std::ifstream file(f_name);
+    if (!file) {
+        std::cerr << "File <" << f_name << "> not found\n";
+    } else {
+        long res{};
+        for (std::string temp; getline(file, temp, ',');) {
+            size_t pos = temp.find('-');
+            long end = stol(temp.substr(pos + 1));
+            for (long i = stol(temp.substr(0, pos)); i <= end; ++i) {
+                std::string num = std::to_string(i);
+                if (num.size() % 2 != 0) { continue; }
+                size_t pos = num.size() / 2;
+                if (num.substr(0, pos) == num.substr(pos)) { 
+                    res += i;
+                }
+            }
+        }
+        std::cout << res;
+    }
+    return 0;
+}
